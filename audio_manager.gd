9793c47c3e2b03da3f91a340bbe6719f73ab6ad2
Justas-Kaulakis <63920269+Justas-Kaulakis@users.noreply.github.com>
2025-03-06 11:21:31 +0200

    NL-51 Tidy project files and structure

--- audio_manager.gd
+++ audio_manager.gd
@@ -7,15 +7,12 @@ var current_area: AudioGlobal.Area
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	current_area = AudioGlobal.current_area
-	
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta) -> void:
 	if current_area != AudioGlobal.current_area:
 		current_area = AudioGlobal.current_area
 		update_music_for_scene()
-
 
 # Cia galima keisti muzika is BackGroundMusicPlayer 
 func update_music_for_scene():
