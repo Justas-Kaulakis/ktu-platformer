b72139cffcdbe877fd674a67f1eddb5f394f866e
JustasHD <justasvilcinskas@gmail.com>
2025-03-16 02:06:18 +0200

    NL-58 Finished level 1 and (hopefully) made poison ivies work as intended

--- level/poison_ivy.gd
+++ level/poison_ivy.gd
@@ -16,15 +16,18 @@ func _on_damage_timer_timeout() -> void:
 	game_manager.update_health(damage)
 
 func _on_body_entered(body: Node2D) -> void:
-	await get_tree().create_timer(delay / 5).timeout
-	game_manager.update_health(damage)
+	if !game_manager.is_poisoned:
+		await get_tree().create_timer(0.1).timeout
+		game_manager.is_poisoned = true
+	else:
+		game_manager.is_poisoned = false
 	timer.start()
 
 func _on_body_exited(body: Node2D) -> void:
 	timer.stop()
-	await get_tree().create_timer(delay).timeout
 	var elapsed_time = 0
-	while elapsed_time < poison_duration:
+	while elapsed_time <= poison_duration and game_manager.is_poisoned:
 		elapsed_time += delay
 		game_manager.update_health(damage)
 		await get_tree().create_timer(delay).timeout
+	game_manager.is_poisoned = false
