345c67fba1cc702b5afb1db2a6ac0db36b9971eb
Karagryb <134153663+Karagryb@users.noreply.github.com>
2025-04-11 20:45:20 +0300

    NL-85-Key-and_Power-up
    pridejau double jump ir wall jump animated sprites tai pat armor jeigu kazkada nuspresim naudot

--- level/powerups/powerup_wall_jump.gd
+++ level/powerups/powerup_wall_jump.gd
@@ -2,6 +2,11 @@ extends Area2D
 
 @onready var game_manager = SceneManager.current_scene.get_node("Game Manager")
 @onready var player = SceneManager.current_scene.get_node("Player")
+@onready var area_2d = $"."
+@onready var animation_player = $"AnimationPlayer"
+
+func _on_ready() -> void:
+	animation_player.play("new_animation")
 
 func _on_body_entered(_body: Node2D) -> void:
 	game_manager.apply_powerup("wall_jump", 5.0)
