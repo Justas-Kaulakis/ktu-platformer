a1b6d8f338af9166d40fe7220cafa632cb968d8c
Karagryb <134153663+Karagryb@users.noreply.github.com>
2025-04-11 21:05:47 +0300

    NL-85-Key-and_Powerups FIX
    pamirsau rakto animacija oops

--- level/doors/key.gd
+++ level/doors/key.gd
@@ -1,6 +1,8 @@
 @tool
 class_name Key extends Area2D
 
+@onready var area_2d = $"."
+@onready var animation_player = $"AnimationPlayer"
 
 ## Color coded key. A key can only open a gate with the same color_name
 @export_enum("white", "yellow", "red", "blue", "green") var color_name: String = "yellow":
@@ -13,3 +15,7 @@ func _on_body_entered(body):
 	if not Engine.is_editor_hint() and body is Player:
 		body.pick_up_key(color_name)
 		queue_free()
+
+
+func _on_ready() -> void:
+	animation_player.play("new_animation")
