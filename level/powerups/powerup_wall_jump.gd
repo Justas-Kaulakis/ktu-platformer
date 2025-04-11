1edc75705b8ec95063efcccaf5a28548271a3704
Justas-Kaulakis <63920269+Justas-Kaulakis@users.noreply.github.com>
2025-04-11 18:40:55 +0300

    Back to main menu

--- level/powerups/powerup_wall_jump.gd
+++ level/powerups/powerup_wall_jump.gd
@@ -1,8 +1,5 @@
 extends Area2D
 
-@onready var game_manager = SceneManager.current_scene.get_node("Game Manager")
-@onready var player = SceneManager.current_scene.get_node("Player")
-
-func _on_body_entered(_body: Node2D) -> void:
-	game_manager.apply_powerup("wall_jump", 5.0)
+func _on_body_entered(player: Player) -> void:
+	player.apply_powerup("wall_jump", 5.0)
 	queue_free()
