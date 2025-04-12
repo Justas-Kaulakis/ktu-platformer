a03d37234025bc5f89e0704e44ac6b682875d963
Teshyo <guyman12330@gmail.com>
2025-04-12 18:37:51 +0300

    NL-83 improved lvl 1

--- level/doors/key.gd
+++ level/doors/key.gd
@@ -1,6 +1,7 @@
 @tool
 class_name Key extends Area2D
-
+var pickable = true
+@onready var player = SceneManager.current_scene.get_node("Player")
 
 ## Color coded key. A key can only open a gate with the same color_name
 @export_enum("white", "yellow", "red", "blue", "green") var color_name: String = "yellow":
@@ -8,8 +9,15 @@ class_name Key extends Area2D
 		color_name = new_color
 		$Sprite2D.modulate = Color(new_color,1)
 
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("reload"):
+		player.consume_key(color_name)
+		pickable = true
+		$Sprite2D.visible = true
 
 func _on_body_entered(body):
-	if not Engine.is_editor_hint() and body is Player:
+	if not Engine.is_editor_hint() and body is Player and pickable:
 		body.pick_up_key(color_name)
-		queue_free()
+		$Sprite2D.visible = false
+		pickable = false
+		#queue_free()
