5b8792d31002500891714b016bdf3996a55b3478
Justas-Kaulakis <63920269+Justas-Kaulakis@users.noreply.github.com>
2025-04-16 19:32:51 +0300

    NL-70 Added platform previews

--- player/platform_gun.gd
+++ player/platform_gun.gd
@@ -1,17 +1,22 @@
 class_name PlatormGun extends Node2D
 
-var selected_platform = Global.p_1
-var platform_count = Global.max_platform_count
-var can_shoot_player = 1 # checks if mouse is on player
-var can_shoot = 1 # checks if mouse is on created platform
+@onready var platform_preview: PlatformPreview = $"../../PlatformPreview"
 @onready var tiles = SceneManager.current_scene.get_node("Layers/Collision Tiles") as TileMapLayer
 @onready var game_manager = SceneManager.current_scene.get_node("Game Manager")
 @onready var player_ui: CanvasLayer = $"../../Player UI"
 
+
+var selected_platform = Global.p_1:
+	set(new_platform):
+		selected_platform = new_platform
+		platform_preview.attach_preview(new_platform)
+var platform_count = Global.max_platform_count
+var can_shoot_player = 1 # checks if mouse is on player
+var can_shoot = 1 # checks if mouse is on created platform
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print(tiles)
-	#pass # Replace with function body.
+	selected_platform = Global.p_1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -42,15 +47,10 @@ func _process(_delta: float) -> void:
 		selected_platform = Global.p_2
 	if Input.is_action_just_pressed("platform_3"):
 		selected_platform = Global.p_3
-		
-		#var _selected_tile = tiles.get_cell_tile_data(_node_coord)
-		#print(node_coord)
-		#if tiles.get_cell_source_id(node_coord) != -1:
-			#print(tiles.get_cell_source_id(node_coord))
-			#print(tiles.get_cell_atlas_coords(node_coord))
-			
-			#tiles.set_cell(node_coord, tiles.get_cell_source_id(Vector2i(15,5)), Vector2i(15,5))
-		
+	
+	if game_manager:
+		platform_preview.handle_drawing(tiles, platform_count, can_shoot, can_shoot_player, game_manager.is_game_paused)
+	
 	if Input.is_action_just_pressed("reload"):
 		reload()
 
---
+++ player/platform_preview.gd
@@ -0,0 +1,39 @@
+class_name PlatformPreview extends Node2D
+
+
+func attach_preview(platform: PackedScene) -> void:
+	# Remove old preview sprite (if any)
+	var vsbl: bool = false
+	
+	# Instantiate a new preview sprite from the platform scene
+	var instance = platform.instantiate()
+	var sprite = instance.get_node("Sprite2D") as Sprite2D
+	instance.remove_child(sprite) # Detach from the full platform instance
+
+	sprite.modulate = Color(0.8, 0.8, 0.8, 0.5) # Gray + transparent
+	sprite.global_position = get_global_mouse_position()
+	sprite.visible = vsbl
+	
+	for child in get_children():
+		vsbl = child.visible
+		child.queue_free()
+	# Add to the preview node
+	sprite.owner = null
+	add_child(sprite)
+
+func handle_drawing(tiles: TileMapLayer, platform_count:int, can_shoot: bool, can_shoot_player: int, is_game_paused) -> void:
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+		var preview:Sprite2D = get_child(0)
+		var global_scaled = tiles.get_global_mouse_position()
+		var node_coord = tiles.local_to_map(tiles.to_local(global_scaled))
+		if platform_count > 0 and can_shoot and can_shoot_player == 1 and \
+			tiles.get_cell_source_id(node_coord) == -1 and !is_game_paused:
+				preview.modulate = Color(0.5, 0.5, 0.5, 0.8)
+		else:
+			preview.modulate = Color(0.9, 0.5, 0.5, 0.8)
+			
+		preview.visible = true
+		preview.global_position = get_global_mouse_position()
+	else:
+		var preview:Sprite2D = get_child(0)
+		preview.visible = false
